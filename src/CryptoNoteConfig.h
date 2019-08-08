83a44a327a460dd3d54acb7d80baa7e24d27cf83
Tarmgas <tarmgas@gmail.com>
2019-08-08 09:41:57 +0200

    add to script
    scripts to make checkpointsfiles

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -2,7 +2,6 @@
 // Copyright (c) 2014-2018, The Monero Project
 // Copyright (c) 2018-2019, The TurtleCoin Developers
 // Copyright (c) 2018-2019, The WrkzCoin developers
-// Copyright (c) 2018-2019, The Coin developers
 // Copyright (c) 2019-2019, The DyngeCoin developers
 //
 // Please see the included LICENSE file for more information.
